23195122975d6aee3a3a26ecb707843f030c43d7
shwetagopaul <shwetagopaul92@gmail.com>
2019-06-06 17:14:43 -0400

    changing monitorSub:

--- inst/shinyApps/TerraShipApp.R
+++ inst/shinyApps/TerraShipApp.R
@@ -18,7 +18,7 @@ require(AnVIL)
 # function to get tool names for the specified project
 getMethodConfigNames <- function(billingworkspace_name, project_name){
   ws = terra$listWorkspaceMethodConfigs(workspaceNamespace = billingworkspace_name,
-                                   workspaceName = project_name,allRepos = TRUE)
+                                        workspaceName = project_name,allRepos = TRUE)
   ws_tool_names = content(ws)
   ws_tool_names
   
@@ -61,7 +61,7 @@ monitorSub <- function(workspaceNamespace, wdlnamespace, name){
 abortSubmission <- function(workspaceNamespace, wdlnamespace, name){
   subDetails = content(terra$listSubmissions(workspaceNamespace,wdlnamespace))
   for(detail in subDetails){
-   mydetail = sapply(subDetails, function(x) {x$methodConfigurationName==name})
+    mydetail = sapply(subDetails, function(x) {x$methodConfigurationName==name})
   }
   mytooldetail = as.data.frame(subDetails[mydetail])
   submissionId = as.character(mytooldetail$submissionId)
@@ -116,8 +116,8 @@ listBullet<-function(mylist,mylistnames){
   }
   return(myhtml)
 }
-  
-  
+
+
 # Setup:
 
 ########################
@@ -144,8 +144,8 @@ TerraShip = function() {
                     ),
                     mainPanel("",width=10,
                               (tags$style(type="text/css",
-                                         ".shiny-output-error { visibility: hidden; }",
-                                         ".shiny-output-error:before { visibility: hidden; }")),
+                                          ".shiny-output-error { visibility: hidden; }",
+                                          ".shiny-output-error:before { visibility: hidden; }")),
                               tabsetPanel(
                                 tabPanel("Tool Configuration",h3("Configuration"),
                                          verbatimTextOutput("tooldetails"),
@@ -161,100 +161,100 @@ TerraShip = function() {
                               )
                     )
       ),
-      ####################
-      # Start Server Config
-      ####################
-      server = function(input, output, session) {
-        
-        # dropdown for billing groups a user belongs to
-        output$billingwsnamespace_dropdown <- renderUI({
-          ws = content(terra$listWorkspaces())
-          mine = sapply(ws, function(x){x$accessLevel=="PROJECT_OWNER"})
-          myws_details = ws[mine]
-          # options for workspace namespace
-          workspacename = lapply(myws_details, function(x) {x$workspace$namespace})
-          # from this get the names avaiable for the chosen billing(workspace) group
-          selectInput("workspaceNamespace", 
-                      "Select Workspace Namespace",
-                      choices = workspacename
-          )
-        })
-        
-        # dropdown for project names available under a billing group
-        output$projectnames_dropdown <- renderUI({
-          ws = content(terra$listWorkspaces())
-          mine = sapply(ws, function(x){x$accessLevel=="PROJECT_OWNER"})
-          myws_details = ws[mine]
-          for(i in myws_details){
-            mybilling = sapply(myws_details, function(x) {x$workspace$namespace==input$workspaceNamespace})
-          }
-          myProjectName = myws_details[mybilling]
-          project_names = lapply(myProjectName, function(x) {x$workspace$name})
-          myProjectNames = as.list(project_names)
-          selectInput("wdlnamespace", 
-                      "Select Project Name",
-                      choices = myProjectNames
-          )
-        })
-        
-        # dropdown for tools names available under a billing group
-        output$toolnames_dropdown <- renderUI({
-          ws = terra$listWorkspaceMethodConfigs(workspaceNamespace = input$workspaceNamespace,
-                                                workspaceName = input$wdlnamespace,allRepos = TRUE)
-          ws_tool_names = content(ws)
-          all_tool_names = lapply(ws_tool_names, function(x) {x$name})
-          selectInput("name", 
-                      "Select Tool",
-                      choices = all_tool_names
-          )
-        })
-        
-        # show selected tool's details
-        observeEvent(input$submitButton, {
-          details = terra$getWorkspaceMethodConfig(input$workspaceNamespace,input$wdlnamespace
-                                                   ,input$wdlnamespace, input$name)
-          tooldetails = content(details)
-          #output$tooldetails = renderUI(HTML(listBullet(tooldetails,names(tooldetails))))
-          
-          #df = list_to_df(tooldetails)
-          #names(df) = c("Value","Name")
-          #reordered_df = df[, c(2,1)]
-          output$tooldetails = renderPrint(str(tooldetails))
-        })
+    ####################
+    # Start Server Config
+    ####################
+    server = function(input, output, session) {
       
+      # dropdown for billing groups a user belongs to
+      output$billingwsnamespace_dropdown <- renderUI({
+        ws = content(terra$listWorkspaces())
+        mine = sapply(ws, function(x){x$accessLevel=="PROJECT_OWNER"})
+        myws_details = ws[mine]
+        # options for workspace namespace
+        workspacename = lapply(myws_details, function(x) {x$workspace$namespace})
+        # from this get the names avaiable for the chosen billing(workspace) group
+        selectInput("workspaceNamespace", 
+                    "Select Workspace Namespace",
+                    choices = workspacename
+        )
+      })
+      
+      # dropdown for project names available under a billing group
+      output$projectnames_dropdown <- renderUI({
+        ws = content(terra$listWorkspaces())
+        mine = sapply(ws, function(x){x$accessLevel=="PROJECT_OWNER"})
+        myws_details = ws[mine]
+        for(i in myws_details){
+          mybilling = sapply(myws_details, function(x) {x$workspace$namespace==input$workspaceNamespace})
+        }
+        myProjectName = myws_details[mybilling]
+        project_names = lapply(myProjectName, function(x) {x$workspace$name})
+        myProjectNames = as.list(project_names)
+        selectInput("wdlnamespace", 
+                    "Select Project Name",
+                    choices = myProjectNames
+        )
+      })
+      
+      # dropdown for tools names available under a billing group
+      output$toolnames_dropdown <- renderUI({
+        ws = terra$listWorkspaceMethodConfigs(workspaceNamespace = input$workspaceNamespace,
+                                              workspaceName = input$wdlnamespace,allRepos = TRUE)
+        ws_tool_names = content(ws)
+        all_tool_names = lapply(ws_tool_names, function(x) {x$name})
+        selectInput("name", 
+                    "Select Tool",
+                    choices = all_tool_names
+        )
+      })
+      
+      # show selected tool's details
+      observeEvent(input$submitButton, {
+        details = terra$getWorkspaceMethodConfig(input$workspaceNamespace,input$wdlnamespace
+                                                 ,input$wdlnamespace, input$name)
+        tooldetails = content(details)
+        #output$tooldetails = renderUI(HTML(listBullet(tooldetails,names(tooldetails))))
         
-        # create and submit jobs 
-        observeEvent(input$runOnTerra, {
-          terra$createSubmission(
-            workspaceNamespace=input$workspaceNamespace,
-            workspaceName=input$wdlnamespace,
-            methodConfigurationNamespace=input$wdlnamespace,
-            methodConfigurationName=input$name,
-            useCallCache=TRUE)
-          showNotification("Job created!")
-        })
-        
-        # monitor job submission
+        #df = list_to_df(tooldetails)
+        #names(df) = c("Value","Name")
+        #reordered_df = df[, c(2,1)]
+        output$tooldetails = renderPrint(str(tooldetails))
+      })
+      
+      
+      # create and submit jobs 
+      observeEvent(input$runOnTerra, {
+        terra$createSubmission(
+          workspaceNamespace=input$workspaceNamespace,
+          workspaceName=input$wdlnamespace,
+          methodConfigurationNamespace=input$wdlnamespace,
+          methodConfigurationName=input$name,
+          useCallCache=TRUE)
+        showNotification("Job created!")
+      })
+      
+      # monitor job submission
+      output$submissionDetails = DT::renderDataTable(monitorSub(input$workspaceNamespace, input$wdlnamespace,
+                                                                input$name), escape=FALSE)
+      
+      # abort job submission
+      observeEvent(input$abortSubmission, {
+        res = abortSubmission(input$workspaceNamespace, input$wdlnamespace, input$name)
+        if(res$status_code != 200){
+          showNotification("Aborting!")
+        }
+        else{
+          showNotification("Aborted!")
+        }
+      })
+      
+      # refresh monitor tab to show updated details
+      observeEvent(input$refreshSubmission, {
         output$submissionDetails = DT::renderDataTable(monitorSub(input$workspaceNamespace, input$wdlnamespace,
                                                                   input$name), escape=FALSE)
-        
-        # abort job submission
-        observeEvent(input$abortSubmission, {
-          res = abortSubmission(input$workspaceNamespace, input$wdlnamespace, input$name)
-          if(res$status_code != 200){
-            showNotification("Aborting!")
-          }
-          else{
-            showNotification("Aborted!")
-          }
-        })
-        
-        # refresh monitor tab to show updated details
-        observeEvent(input$refreshSubmission, {
-          output$submissionDetails = DT::renderDataTable(monitorSub(input$workspaceNamespace, input$wdlnamespace,
-                                                                    input$name), escape=FALSE)
-        })
-        
-        
-  }
-)}
+      })
+      
+      
+    }
+    )}
